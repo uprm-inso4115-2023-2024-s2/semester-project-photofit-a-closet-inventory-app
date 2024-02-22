2e63b4fc4372c783a32b34548d75b59e753471b7
Diego A. Velez Torres <diego.velez6@upr.edu>
2024-02-22 10:30:32 -0400

    Remove useless functions and refactor (tabs)/_layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,91 +1,70 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import {Link, Tabs} from 'expo-router';
+import {Pressable} from 'react-native';
 
 import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
+import {useColorScheme} from '@/components/useColorScheme';
+import {useClientOnlyValue} from '@/components/useClientOnlyValue';
 
 //hanger icon import
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 //man outline import
-import { Ionicons } from '@expo/vector-icons';
-
+import {Ionicons} from '@expo/vector-icons';
 
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
-function TabBarIcon3(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <MaterialCommunityIcons name="hanger" size={24} color="black" />;
-}
-
-function TabBarIcon2(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <Ionicons name="man-outline" size={24} color="black" />;
-}
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                // Disable the static render of the header on web
+                // to prevent a hydration error in React Navigation v6.
+                headerShown: useClientOnlyValue(false, true),
+            }}>
 
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Outfits',
-          tabBarIcon: ({ color }) => <TabBarIcon2 name="man-outline" color={color} />,
-        }}
-      />
+            <Tabs.Screen
+                name="two"
+                options={{
+                    title: 'Outfits',
+                    tabBarIcon: ({color}) => <Ionicons name="man-outline" size={24} color={color}/>,
+                }}
+            />
 
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    // tabBarIcon: ({color}) => <TabBarIcon name="home" color={color}/>,
+                    tabBarIcon: ({color}) => <FontAwesome name="home" size={28} style={{marginBottom: -3}}
+                                                          color={color}/>,
+                    headerRight: () => (
+                        <Link href="/modal" asChild>
+                            <Pressable>
+                                {({pressed}) => (
+                                    <FontAwesome
+                                        name="info-circle"
+                                        size={25}
+                                        color={Colors[colorScheme ?? 'light'].text}
+                                        style={{marginRight: 15, opacity: pressed ? 0.5 : 1}}
+                                    />
+                                )}
+                            </Pressable>
+                        </Link>
+                    ),
+                }}
+            />
 
-      <Tabs.Screen
-        name="three"
-        options={{
-          title: 'Closet',
-          tabBarIcon: ({ color }) => <TabBarIcon3 name="hanger" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+            <Tabs.Screen
+                name="three"
+                options={{
+                    title: 'Closet',
+                    tabBarIcon: ({color}) => <MaterialCommunityIcons name="hanger" size={24} color={color}/>,
+                }}
+            />
+        </Tabs>
+    );
 }
