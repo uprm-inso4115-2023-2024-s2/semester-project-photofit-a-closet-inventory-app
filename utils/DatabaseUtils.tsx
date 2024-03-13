cc8588784e0323eadc705140e78c6e12f99943b7
Diego A. Velez Torres <diego.velez6@upr.edu>
2024-03-13 12:12:26 -0400

    Modified DatabaseUtils in order to use clothe's properties within the Clothe object

--- utils/DatabaseUtils.tsx
+++ utils/DatabaseUtils.tsx
@@ -1,5 +1,6 @@
 import * as FileSystem from 'expo-file-system';
 import * as SQLite from 'expo-sqlite';
+import {Clothe, Type} from "@/classes/clothe"
 
 /**
  * Opens the database (closet.db) and creates a clothes table if it doesn't exist.
@@ -20,22 +21,19 @@ async function openDatabase(): Promise<SQLite.SQLiteDatabase> {
 
 /**
  * Inserts a clothe object into the clothes table.
- * @param name The name of the clothe.
- * @param description The description of the clothe.
- * @param link The link of the clothe image.
  * @param clothe The clothe object or null.
- * @return Returns true is it successfully added the clothe to the DB, false otherwise.
+ * @return Returns true if it successfully added the Clothe object to the DB, false otherwise.
  */
-export async function insertClothe(name: string, description: string, link: string, clothe: Clothe | null): Promise<boolean> {
-    const db = await openDatabase();
+export async function insertClothe(clothe: Clothe): Promise<boolean> {
+    const db = SQLite.openDatabase('closet.db');
     const serializedClothe = JSON.stringify(clothe);
     let success = false;
 
-    console.log(`Saving ${name}, ${description}, ${link} and ${serializedClothe} to clothes table`)
+    console.log(`Saving ${serializedClothe} to clothes table`)
 
     await db.transactionAsync(async tx => {
-        const result = await tx.executeSqlAsync('INSERT INTO clothes (name, description, link, clothe) VALUES (?, ?, ?, ?)',
-            [name, description, link, serializedClothe]);
+        const result = await tx.executeSqlAsync('INSERT INTO clothes (clothe) VALUES (?)',
+            [serializedClothe]);
         success = result.rowsAffected > 0;
     }, false);
 
@@ -45,25 +43,14 @@ export async function insertClothe(name: string, description: string, link: stri
 /**
  * Gets all the clothes from the DB.
  */
-export async function getClothes(): Promise<{ [key: string]: string | number | null | Clothe }[]> {
+export async function getClothes(): Promise<Clothe[]> {
     const db = await openDatabase();
-    let clothes: { [key: string]: string | number | null | Clothe }[] = [];
+    let clothes: Clothe[] = [];
 
     await db.transactionAsync(async tx => {
         const clotheRows = (await tx.executeSqlAsync('SELECT * FROM clothes')).rows;
         clothes = clotheRows.map((value, index, array) => {
-            return {
-                "id": value.id,
-                "name": value.name,
-                "description": value.description,
-                "link": value.link,
-                "clothe": JSON.parse(value.clothe, (key, value) => {
-                    if (value === null) {
-                        return null;
-                    }
-                    return new Clothe(value.type, value.color, value.sleeveSize);
-                })
-            };
+            return JSON.parse(value.clothe);
         });
     }, true);
 
@@ -77,12 +64,9 @@ export async function getClothes(): Promise<{ [key: string]: string | number | n
  * @param sleeveSize The sleeve size of the clothes to filter.
  * @return The filtered clothes based on the filters passed.
  */
-export async function filterClothes(type?: string, color?: string, sleeveSize?: number): Promise<{
-    [key: string]: string | number | null | Clothe
-}[]> {
+export async function filterClothes(type?: Type, color?: string, sleeveSize?: number): Promise<Clothe[]> {
     return (await getClothes())
-        .filter((value) => {
-            const clothe = value.clothe as Clothe
+        .filter((clothe) => {
             let shouldFilter = true;
 
             if (type !== undefined) {
