5b44ca095d1f7deea002337eec14e6819d4f3560
Diego A. Velez Torres <diego.velez6@upr.edu>
2024-03-19 10:34:48 -0400

    Create Filter component and add it to closet screen

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -1,10 +1,11 @@
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {KeyboardAvoidingView, ScrollView, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {View} from '@/components/Themed';
 import {useEffect, useState} from "react";
 import DatabaseController from "@/classes/DatabaseController";
 import ClotheComponent from "@/components/ClotheComponent";
 import {newClotheAddedTrigger} from "@/app/addClothe";
 import {Clothe} from "@/classes/clothe";
+import Filter from "@/components/Filter";
 
 // Closet screen
 export default function TabThreeScreen() {
@@ -32,38 +33,51 @@ export default function TabThreeScreen() {
 
     if (clothes.length === 0) {
         return (
-            <View style={styles.container}>
+            <View style={{flex: 1, alignItems: "center"}}>
                 <Text>No clothes saved</Text>
             </View>
         )
     }
 
     return (
-        <View style={styles.container}>
-            {/* The ClotheComponent cycler */}
-            <View style={styles.cyclerContainer}>
-                <ClotheComponent clothe={clothes[clotheIndex]}/>
+        // Everything was wrapped inside a KeyboardAvoidingView inside a ScrollView (as per
+        // https://forums.expo.dev/t/problems-with-keyboardavoidview/7799) in order to avoid the UI getting messed up.
+        // The ClotheComponent still moves a little thou when the keyboard pops up
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <KeyboardAvoidingView style={styles.keyboardContainer}>
+                <Filter/>
 
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button} onPress={handlePrevious}>
-                        <Text style={styles.buttonText}>Previous</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={handleNext}>
-                        <Text style={styles.buttonText}>Next</Text>
-                    </TouchableOpacity>
+                {/* The clothes cycler */}
+                <View style={styles.cyclerContainer}>
+                    <ClotheComponent clothe={clothes[clotheIndex]}/>
+
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={styles.button} onPress={handlePrevious}>
+                            <Text style={styles.buttonText}>Previous</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.button} onPress={handleNext}>
+                            <Text style={styles.buttonText}>Next</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
-        </View>
+            </KeyboardAvoidingView>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    scrollContainer: {
+        flexGrow: 1,
+        justifyContent: "center",
+        backgroundColor: "white"
+    },
+    keyboardContainer: {
         flex: 1,
-        alignItems: 'center',
+        alignItems: "center",
+        backgroundColor: "white"
     },
     cyclerContainer: {
-        flex: 1,
+        flex: 0.9,
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: undefined,
